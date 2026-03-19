853c323ff785cfc4541f77efc6b5a1ddf8e41e7f
Derbys-Manuel <derbysmanuel@gmail.com>
2026-03-19 16:04:37 -0500

    ok hot fix purchases edit

--- src/modules/purchases/domain/ports/purchase-order-item.port.repository.ts
+++ src/modules/purchases/domain/ports/purchase-order-item.port.repository.ts
@@ -6,5 +6,6 @@ export const PURCHASE_ORDER_ITEM = Symbol("PURCHASE_ORDER_ITEM");
 export interface PurchaseOrderItemRepository {
   add(item: PurchaseOrderItem, tx?: TransactionContext): Promise<PurchaseOrderItem>;
   remove(poItemId: string, tx?: TransactionContext): Promise<boolean>;
+  removeByPurchaseId(poId: string, tx?: TransactionContext): Promise<number>;
   getByPurchaseId(poId: string, tx?: TransactionContext): Promise<PurchaseOrderItem[]>;
 }
